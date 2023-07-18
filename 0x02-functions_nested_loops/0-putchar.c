8625f59055d315a558cfb395d36dfaae47c53de7
Abraham Idris <ab4excel@gmail.com>
2023-07-18 05:39:14 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,19 +7,12 @@
 int main(void)
 {
 	_putchar('_');
-    	_putchar('\n');
     	_putchar('p');
-    	_putchar('\n');
     	_putchar('u');
-    	_putchar('\n');
     	_putchar('t');
-    	_putchar('\n');
     	_putchar('c');
-    	_putchar('\n');
     	_putchar('h');
-    	_putchar('\n');
     	_putchar('a');
-    	_putchar('\n');
     	_putchar('r');
     	_putchar('\n');	
 
