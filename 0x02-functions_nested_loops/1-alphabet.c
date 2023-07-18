69cab16e499e29aa614371950fa6e16411a9025f
Abraham Idris <ab4excel@gmail.com>
2023-07-18 05:57:12 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints alphabets
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
