5fc5cba0388de773f66d72a3d475e2772fb59eba
Abraham Idris <ab4excel@gmail.com>
2023-07-18 05:05:15 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print putchar
+ *
+ * Returns - 0
+ */
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('5');
+
+	return (0);
+}
