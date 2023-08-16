b8989100aca2d8c5fcb2688b6ea144512d7d6fa7
Abraham Idris <ab4excel@gmail.com>
2023-08-16 13:05:00 -0700

    quick_fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,13 @@
 #include <stddef.h>
 
 /**
- * array_iterator - This function iterates through an array and applies an action to each element.
+ * array_iterator - This function iterate through an array
  *
  * @array: Pointer to the array of integers.
  * @size: Number of elements in the array.
  * @action: Function pointer to the action to be applied to each element.
- * Description: This function iterates through the given array and applies the provided action to each element.
- *              The action is a function that takes an integer argument.
+ * Description: This function iterates through the given array
+ *
  * Return: This is a void function, no return.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
