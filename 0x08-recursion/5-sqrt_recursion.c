c830a73f63e927caeb7af9ee861cf65782cc56f8
iahmab92 <ab4excel@gmail.com>
2023-09-28 02:46:16 -0700

    function that returns the sqaure root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,24 +5,24 @@
  * @a: same number as n
  * @b: number that iterates from 1 to n
  *
- * Return: On success 1
+ * Return: On success 1.
  */
-int sqrt_(int x, int y)
+int sqrt2(int a, int b)
 {
-	if (y * y == x)
-		return (y);
-	else if (y * y > x)
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
 		return (-1);
-	else		
-		return (sqrt2(x, y + 1));
+	else
+		return (sqrt2(a, b + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of n
  * @n: Number Integer
  *
- * Return: On success 1
+ * Return: On success 1.
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrt_(n, 1));
+	return (sqrt2(n, 1));
 }
