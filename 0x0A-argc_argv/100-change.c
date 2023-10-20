8b906048cccef8d58884e20b2a5b6b27d2a01cc2
iahmab92 <ab4excel@gmail.com>
2023-10-20 05:45:20 -0700

    a program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -43,7 +43,7 @@ int coinConverter(int i)
  * main - Takes in exactly one argument for minimum coin count
  * @argc: Number of command line arguments
  * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * Return: 0 if exactly 1 argument is passed into this program, 1 
  */
 int main(int argc, char *argv[])
 {
